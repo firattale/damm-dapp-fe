6d6c94579f6e0150b8d94ead25d72ca42b22bd17
firatstory.eth <talefirat@gmail.com>
2022-12-29 18:29:02 +0100

    initial commit and cleanup of boilerplate

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -10,28 +10,28 @@ import App from "./App";
 // IMPORTANT, PLEASE READ
 // To avoid disruptions in your app, change this to your own Infura project id.
 // https://infura.io/register
-const INFURA_PROJECT_ID = "529670718fd74cd2a041466303daecd7";
+const INFURA_PROJECT_ID = "8b60443ba0a642fd90aa5f5799b0321c";
 const config = {
-  readOnlyChainId: Mainnet.chainId,
-  readOnlyUrls: {
-    [Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
-  },
-}
+	readOnlyChainId: Mainnet.chainId,
+	readOnlyUrls: {
+		[Mainnet.chainId]: "https://mainnet.infura.io/v3/" + INFURA_PROJECT_ID,
+	},
+};
 
 // You should replace this url with your own and put it into a .env file
 // See all subgraphs: https://thegraph.com/explorer/
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
+	cache: new InMemoryCache(),
+	uri: "https://api.thegraph.com/subgraphs/name/paulrberg/create-eth-app",
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DAppProvider config={config}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </DAppProvider>
-  </React.StrictMode>,
-  document.getElementById("root"),
+	<React.StrictMode>
+		<DAppProvider config={config}>
+			<ApolloProvider client={client}>
+				<App />
+			</ApolloProvider>
+		</DAppProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
